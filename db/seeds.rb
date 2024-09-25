26fea813db0a463e4ef9c8e827a59f42d83b930f
Adelina Gutu <w1705972@gmail.com>
2024-09-25 17:09:23 +0100

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.create([
+  { name: "Wok and Roll", address: "123 Noodle St", phone_number: "123-456-7890", category: "chinese" },
+  { name: "Pizza Palace", address: "456 Cheese Ave", phone_number: "987-654-3210", category: "italian" },
+  { name: "Sushi Spot", address: "789 Fish Blvd", phone_number: "555-123-4567", category: "japanese" },
+  { name: "Bistro Belle", address: "321 French Way", phone_number: "222-333-4444", category: "french" },
+  { name: "Waffle House", address: "654 Waffle St", phone_number: "888-999-0000", category: "belgian" }
+])
